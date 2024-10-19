c26538b7d942b33526334d16a3b3ff8a0daf5183
Cristobal <cristobal230502@gmail.com>
2024-10-19 18:44:07 -0300

    Agregado de carpeta sources para imagenes del Juego. Traida una imagen y hecha mostrar en pantalla en el main del cliente, esto funciona. A su vez, se agrega dependencias en el Cmake para buscar la libreria de SDL2 de imagenes para poder procesarlas.

--- client/main.cpp
+++ client/main.cpp
@@ -3,35 +3,46 @@
 
 #include <SDL2pp/SDL2pp.hh>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 using namespace SDL2pp;
 
 int main() try {
+
 	// Initialize SDL library
 	SDL sdl(SDL_INIT_VIDEO);
 
-	// Create main window: 640x480 dimensions, resizable, "SDL2pp demo" title
-	Window window("SDL2pp demo",
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			640, 480,
-			SDL_WINDOW_RESIZABLE);
+	 // Initialize SDL_image
+    IMG_Init(IMG_INIT_PNG);
 
-	// Create accelerated video renderer with default driver
-	Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+	// Create main window
+    SDL2pp::Window window("SDL2 Image Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+    
+    // Create renderer
+    SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	// Clear screen
-	renderer.Clear();
+    SDL2pp::Surface loadedSurface(IMG_Load("../src/Grey-Duck.png"));
+    
+    // Create texture from surface using SDL2pp::Texture
+    SDL2pp::Texture texture(renderer, loadedSurface);
 
-	// Show rendered frame
-	renderer.Present();
+    // Clear screen
+    renderer.Clear();
 
-	// 5 second delay
-	SDL_Delay(5000);
+    // Copy texture to renderer
+    renderer.Copy(texture);
 
-	// Here all resources are automatically released and library deinitialized
-	return 0;
+    // Show rendered frame
+    renderer.Present();
+
+    // 5 second delay
+    SDL_Delay(5000); 
+
+    // Clean up SDL_image
+    IMG_Quit();
+    return 0;
 } catch (std::exception& e) {
 	// If case of error, print it and exit with error
 	std::cerr << e.what() << std::endl;
-	return 1;
+	
 }
