450d1b2663a11bfb9f742c86c2f093565d28afe0
Lucas Ruiz <lruiz@matchfin.ar>
2024-10-22 19:41:29 -0300

    Agregado de el protocolo del server con herencia del common, falta todavia agregar el envio del estado del juego pero aun tenemos dudas para preguntar antes de hacer eso

--- server/server_protocolo.h
+++ server/server_protocolo.h
@@ -4,29 +4,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+#include "../common/common_protocolo.h"
 #include "../common/common_queue.h"
 #include "../common/common_socket.h"
 
-class ServerProtocolo {
+struct Comando {
+    uint8_t comando;
+    uint8_t accion;
+    uint8_t jugador_id;
+};
+
+class ServerProtocolo : public Protocolo{
 private:
-    Socket& socket;
-    bool* esta_cerrado;
-
-    std::vector<uint8_t> recibir_nombre();
+    Comando recibir_nombre();
     uint8_t recibir_caja();
-
+    
 public:
-    ServerProtocolo(Socket& socket, bool* esta_cerrado);
-
-    /*0x06 0x04 <player-name> <reward-id>, donde el <reward-id> es un byte con el id de la
-    recompensa. Este método indica que un jugador recogió el premio de una caja.*/
-    int enviar_reward(std::string& nombre, uint8_t* reward_id);
-
-    /*0x06 0x05 para indicar que una caja reapareció.*/
-    int enviar_nueva_caja();
-
-    void recibir_pickup(Queue<std::string>* recibidos);
+    ServerProtocolo(Socket& socket);
+    Comando recibir_comando();
 };
 
 #endif
