4c4e7b9bf8d39197af64ae0afea1b55868edff0c
Lucas Ruiz <lruiz@matchfin.ar>
2024-10-24 20:49:34 -0300

    Creo una clase entidad generica de la cual heredaran todos los elementos que se muestren en el mapa

---
+++ common/common_entidad.h
@@ -0,0 +1,18 @@
+#ifndef ENTIDAD_H
+#define ENTIDAD_H
+
+#include <cstdint>
+
+class Entidad {
+    protected:
+        uint8_t id;
+        uint8_t pos_x;
+        uint8_t pos_y;
+    public:
+        Entidad(uint8_t id, uint8_t pos_x, uint8_t pos_y);
+        uint8_t get_id();
+        uint8_t get_pos_x();
+        uint8_t get_pos_y();
+};
+
+#endif
