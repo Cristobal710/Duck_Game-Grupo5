fbd86ad74605cf1933cf9df0c9d9a63041f7afd8
Lucas Ruiz <lruiz@matchfin.ar>
2024-10-27 23:44:28 -0300

    Agregado de constantes en un archivo separado y modificacion del struct estado de juego

--- common/common_estado_juego.h
+++ common/common_estado_juego.h
@@ -2,11 +2,20 @@
 #define COMMON_ESTADO_JUEGO_H
 
 #include <cstdint>
-#include <vector>
+#include <list>
+#include "common_pato.h"
+#include "common_caja.h"
+#include "common_arma.h"
+#include "common_bala.h"
+#include "common_granada.h"
+
 
 struct EstadoJuego {
-    std::vector<uint8_t> estado;
+    std::list<Pato> patos;
+    std::list<Caja> cajas;
+    std::list<Arma> armas;
+    std::list<Bala> balas;
+    std::list<Granada> granadas;
 };
-
 
 #endif
