83fc7100275c677d523046c904773f5de28ca3f5
cartchera <cbasso@fi.uba.ar>
2024-11-08 12:37:04 -0300

    se puede ver en la interfaz al pato con un arma en la mano (falta verificar animacion de las balas)

--- client/movimiento_lateral.cpp
+++ client/movimiento_lateral.cpp
@@ -78,25 +78,3 @@ void MovimientoLateral::pato_movimiento(uint8_t& movimiento, uint8_t& direccion_
     }
 }
 
-
-/*void MovimientoLateral::cargar_frames(SDL2pp::Renderer& renderer, SDL2pp::Surface& sprite_sheet,
-                                 int offset_y, std::vector<SDL2pp::Texture>& texturas,
-                                 int num_frames) {
-    for (int i = 0; i < num_frames; ++i) {
-
-        SDL_Rect rect_inicial = {i * PIXEL_PATO, offset_y, PIXEL_PATO, PIXEL_PATO};
-
-        SDL2pp::Surface sprite_superficie(
-                SDL_CreateRGBSurface(0, PIXEL_PATO, PIXEL_PATO, 32, 0, 0, 0, 0));
-
-        SDL_BlitSurface(sprite_sheet.Get(), &rect_inicial, sprite_superficie.Get(), nullptr);
-
-        Uint32 color_key = SDL_MapRGB(sprite_superficie.Get()->format, 0, 0, 0);
-
-        SDL_SetColorKey(sprite_superficie.Get(), SDL_TRUE, color_key);
-
-        SDL2pp::Texture sprite_textura(renderer, sprite_superficie);
-
-        texturas.emplace_back(std::move(sprite_textura));
-    }
-}*/
--- client/movimiento_salto.cpp
+++ client/movimiento_salto.cpp
@@ -50,27 +50,3 @@ void MovimientoSalto::pato_salta(uint8_t& movimiento, int pos_x, int pos_y, int 
     //    puntero_caida = (puntero_caida + 1) % NUM_FRAMES_CAE_PATO;
     //}
 }
-
-/*void MovimientoSalto::cargar_frames(SDL2pp::Renderer& renderer, SDL2pp::Surface& sprite_sheet,
-                                 int offset_y, std::vector<SDL2pp::Texture>& texturas,
-                                 int num_frames) {
-    for (int i = 0; i < num_frames; ++i) {
-
-        SDL_Rect rect_inicial = {i * PIXEL_PATO, offset_y, PIXEL_PATO, PIXEL_PATO};
-
-        SDL2pp::Surface sprite_superficie(
-                SDL_CreateRGBSurface(0, PIXEL_PATO, PIXEL_PATO, 32, 0, 0, 0, 0));
-
-        SDL_BlitSurface(sprite_sheet.Get(), &rect_inicial, sprite_superficie.Get(), nullptr);
-
-        Uint32 color_key = SDL_MapRGB(sprite_superficie.Get()->format, 0, 0, 0);
-
-        SDL_SetColorKey(sprite_superficie.Get(), SDL_TRUE, color_key);
-
-        SDL2pp::Texture sprite_textura(renderer, sprite_superficie);
-
-        texturas.emplace_back(std::move(sprite_textura));
-    }
-}*/
-
-
