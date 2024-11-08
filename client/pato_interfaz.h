d4f78554f6600b7048a2feee0d26d32fcebc4da9
cartchera <cbasso@fi.uba.ar>
2024-11-08 09:42:04 -0300

    se eliminan funciones en 'pato_interfaz' que ya no se utilizan

--- client/pato_interfaz.h
+++ client/pato_interfaz.h
@@ -13,36 +13,12 @@ private:
     MovimientoLateral movimiento_pato_lateral;
     MovimientoSalto movimiento_pato_salto;
     MovimientoAgachado movimiento_pato_agachado;
-    std::vector<SDL2pp::Texture> movimiento_pato;
-    std::vector<SDL2pp::Texture> salto_pato;
-    std::vector<SDL2pp::Texture> acostarse_pato;
-    int puntero_movimiento; 
-
-    void frames_movimientos(SDL2pp::Renderer& renderer, SDL2pp::Surface& sprite_sheet);
-    void frames_salto(SDL2pp::Renderer& renderer, SDL2pp::Surface& sprite_sheet);
-    void frames_acostarse(SDL2pp::Renderer& renderer, SDL2pp::Surface& sprite_sheet);
-    void cargar_frames(SDL2pp::Renderer& renderer, SDL2pp::Surface& sprite_sheet, int offset_y,
-                       std::vector<SDL2pp::Texture>& texturas, int num_frames);
-
-    SDL2pp::Texture& mostrar_frame_derecha();
 
 public:
     PatoInterfaz(SDL2pp::Renderer& renderer, const std::string& pato_path);
 
-    void pato_camina_derecha(SDL2pp::Renderer& renderer, SDL_Rect& rect_inicio,
-                             SDL2pp::Rect& rect_destino);
-
-    void pato_camina_izquierda(SDL2pp::Renderer& renderer, SDL_Rect& rect_inicio,
-                               SDL2pp::Rect& rect_destino);  // Nueva declaración
-
-    void pato_salta(SDL2pp::Renderer& renderer, SDL_Rect& rect_inicio, SDL2pp::Rect& rect_destino);
-
-    void pato_agachado(SDL2pp::Renderer& renderer, SDL_Rect& rect_inicio,
-                       SDL2pp::Rect& rect_destino);
-
-    SDL2pp::Texture& mostrar_frame();
-
     void dibujar(uint8_t& estado_pato_movimiento, uint8_t& estado_pato_salto, uint8_t& direccion_pato, uint8_t& se_tira_al_piso, int pos_x, int pos_y, int it);
+
 };
 
 #endif  // PATO_INTERFAZ_H
