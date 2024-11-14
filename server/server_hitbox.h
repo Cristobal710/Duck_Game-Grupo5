e79f054d03ba9c86cf6b4ae56b5ea7bc79ff069c
dalmiroVilaplana <dalmirov@epidataconsulting.com>
2024-11-14 14:26:06 -0300

    clase hitbox

---
+++ server/server_hitbox.h
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+
+
+
+
+class HitBox{
+    public:
+    HitBox(uint16_t x, uint16_t y, uint16_t largo, uint16_t ancho);
+    SDL_Point superior_izq;
+    SDL_Point superior_der;
+    SDL_Point inferior_izq;
+    SDL_Point inferior_der;
+
+}
